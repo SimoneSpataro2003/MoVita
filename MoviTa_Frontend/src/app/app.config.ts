268bd6d6fe3fbbca239c4534791a3fc89405bb72
garra <garrafa435@gmail.com>
2025-01-04 20:04:58 +0100

    Realizzato un primo scheletro della homepage. I pulsanti non funzionano.

---
+++ MoviTa_Frontend/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(NgbModule)]
+};
