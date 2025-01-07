f2df818afa361fc4568439b3d49290859ddab433
samuele <samuelerovito002@gmail.com>
2025-01-07 09:05:30 +0100

    realizzato la struttura della parte back-end del progetto, implementato payment e user (con relativi dao), UserProxy, e primi accenni di gestione delle exception

---
+++ MoVita_BackEnd/src/main/java/org/example/movita_backend/MoVitaBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.movita_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoVitaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoVitaBackEndApplication.class, args);
+    }
+
+}
