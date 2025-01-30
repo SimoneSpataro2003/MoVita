f756be63f7bd9d9b744632bf82c6dd5c547b66a9
garra <garrafa435@gmail.com>
2025-01-30 12:03:49 +0100

    realizzato CategoryProxy

--- MoVita_BackEnd/src/main/java/org/example/movita_backend/persistence/proxy/CategoryProxy.java
+++ MoVita_BackEnd/src/main/java/org/example/movita_backend/persistence/proxy/CategoryProxy.java
@@ -1,20 +1,28 @@
 package org.example.movita_backend.persistence.proxy;
 
-import org.example.movita_backend.model.Booking;
-import org.example.movita_backend.model.Category;
-import org.example.movita_backend.model.Review;
-import org.example.movita_backend.model.User;
+import org.example.movita_backend.model.*;
 import org.example.movita_backend.persistence.DBManager;
 
 import java.sql.Connection;
 import java.util.List;
 
-//todo: terminare
 public class CategoryProxy extends Category {
 
-    public CategoryProxy() {
-
+    public CategoryProxy(Category category) {
+        this.id = category.getId();
+        this.nome = category.getNome();
+        this.descrizione = category.getDescrizione();
     }
 
+    @Override
+    public List<User> getUtentiInteressati(){
+        this.utentiInteressati = DBManager.getInstance().getCategoryDAO().findUsers(this);
+        return utentiInteressati;
+    }
 
+    @Override
+    public List<Event> getEventi(){
+        this.eventi = DBManager.getInstance().getCategoryDAO().findEvents(this);
+        return eventi;
+    }
 }
