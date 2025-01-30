be41248f01a5cd85a910a5d4ca65d71bc64c1039
Giuseppe <giuseppenucifora2703@gmail.com>
2025-01-30 16:03:56 +0100

    modificato Event Service per Event Proxy

--- MoVita_BackEnd/src/main/java/org/example/movita_backend/controller/EventController.java
+++ MoVita_BackEnd/src/main/java/org/example/movita_backend/controller/EventController.java
@@ -41,20 +41,20 @@ public class EventController {
         return ResponseEntity.ok(allEvents);
     }
 
-    @GetMapping("/get-event-by-id")
-    ResponseEntity<Event> getEventById(@RequestParam(name = "eventId") int eventId){
+    @GetMapping("/get-event-by-id/eventId")
+    ResponseEntity<Event> getEventById(@PathVariable int eventId){
         Event event = eventService.findById(eventId);
         return ResponseEntity.ok(event);
     }
 
-    @GetMapping("/get-event-by-filter")
-    ResponseEntity<Collection<Event>> getEventByFilter(@RequestParam(name = "filter")  String filter){
+    @GetMapping("/get-event-by-filter/filter")
+    ResponseEntity<Collection<Event>> getEventByFilter(@PathVariable String filter){
         Collection<Event> events = eventService.findByFilter(filter);
         return ResponseEntity.ok(events);
     }
 
-    @GetMapping("/get-event-by-user")
-    ResponseEntity<Collection<Booking>> getEventByUser(@RequestParam(name = "user")  int user){
+    @GetMapping("/get-event-by-user/user")
+    ResponseEntity<Collection<Booking>> getEventByUser(@PathVariable int user){
         Collection<Booking> events = bookingService.findByUser(user);
         return ResponseEntity.ok(events);
     }
