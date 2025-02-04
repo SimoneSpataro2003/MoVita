e0ff39491bf4bae57dc72426eccaeca215f4fd33
Giuseppe <giuseppenucifora2703@gmail.com>
2025-02-04 11:04:59 +0100

    details in progres

--- MoviTa_Frontend/src/app/pages/details/eventi-simili/eventi-simili.component.ts
+++ MoviTa_Frontend/src/app/pages/details/eventi-simili/eventi-simili.component.ts
@@ -22,9 +22,9 @@ export class EventiSimiliComponent {
       let categorie: Categoria[] = [];
       this.eventService.getCategories(idEvento).subscribe({
         next: (data) => {
-          categorie = data;          
-          console.log(data);          
-          this.ottieniEventi(idEvento,categorie);
+          categorie = data;
+          console.log(data);
+          this.ottieniEventi(idEvento, categorie);
         },
         error: (err) => {
           console.error('Errore nel recupero CATEGORIE evento', err);
@@ -34,41 +34,30 @@ export class EventiSimiliComponent {
     }
   }
 
-  ottieniEventi(idEvento:number,categorie: Categoria[]): void {
-
+  ottieniEventi(idEvento: number, categorie: Categoria[]): void {
+    let eventiS = new Set<number>(); // Set di ID per il controllo dei duplicati
+   
     console.log("CATEGORIE ARRIVATE");
+
     for (let i of categorie) {
       if (idEvento) {
         this.categoryService.findEventsByCategory(String(i.id)).subscribe({
           next: (data) => {
-            const isUnique = (data: Evento[]) => {
-              for (let dataItem of data) {
-                for (let eventItem of this.eventiSimili) {
-                  console.log(`${eventItem.id} ${dataItem.id}`)
-                  if (eventItem.id == dataItem.id)
-                    return false;
-                }
+            for (let d of data) {
+              if (!eventiS.has(d.id)) {  // Controllo basato sull'id
+                eventiS.add(d.id);
+                this.eventiSimili.push(d);
               }
-              return true;
-            };
-
-            if (isUnique(data)) {
-              this.eventiSimili = this.eventiSimili.concat(data);
-              console.log("Eventi simili")
-              console.log(this.eventiSimili);
-              console.log("Eventi simili")
-
-
-            }
-
+            }           
           },
           error: (err) => {
             console.error('Errore nel recupero eventi simili', err);
-
           }
         });
       }
+    
     }
+
 
 
   }
