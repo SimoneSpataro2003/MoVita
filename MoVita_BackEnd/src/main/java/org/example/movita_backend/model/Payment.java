782d2dfce410c37ccf336bed7e41a6abaf05274e
samuele <samuelerovito002@gmail.com>
2025-02-04 16:53:08 +0100

    push di aggiornamento

--- MoVita_BackEnd/src/main/java/org/example/movita_backend/model/Payment.java
+++ MoVita_BackEnd/src/main/java/org/example/movita_backend/model/Payment.java
@@ -1,8 +1,12 @@
 package org.example.movita_backend.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class Payment {
     protected int id;
     protected String titolo;
@@ -21,43 +25,4 @@ public class Payment {
         this.id_utente = id_utente;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitolo() {
-        return titolo;
-    }
-
-    public void setTitolo(String titolo) {
-        this.titolo = titolo;
-    }
-
-    public long getAmmontare() {
-        return ammontare;
-    }
-
-    public void setAmmontare(int ammontare) {
-        this.ammontare = ammontare;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public int getId_utente() {
-        return id_utente;
-    }
-
-    public void setId_utente(int id_utente) {
-        this.id_utente = id_utente;
-    }
 }
