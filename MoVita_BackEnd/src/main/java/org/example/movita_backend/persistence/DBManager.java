0152c9f07eefa210752e0e64980da03f99784a28
Giuseppe <giuseppenucifora2703@gmail.com>
2025-02-04 11:38:51 +0100

    mod

--- MoVita_BackEnd/src/main/java/org/example/movita_backend/persistence/DBManager.java
+++ MoVita_BackEnd/src/main/java/org/example/movita_backend/persistence/DBManager.java
@@ -32,7 +32,7 @@ public class DBManager {
         if (connection == null){
             try {
                 connection = DriverManager.getConnection(
-                        "jdbc:postgresql://localhost:5432/movita_db", "postgres", "221003");
+                        "jdbc:postgresql://localhost:5432/movita", "postgres", "postgres");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
