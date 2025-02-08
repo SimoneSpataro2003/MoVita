066cf569cfb01e085831d207b4b3d7f8c98f602c
Giuseppe <giuseppenucifora2703@gmail.com>
2025-02-08 13:03:20 +0100

    Aggiustato bug immagini

--- MoviTa_Frontend/src/app/shared/common/event-card/event-card.component.ts
+++ MoviTa_Frontend/src/app/shared/common/event-card/event-card.component.ts
@@ -1,18 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Evento} from '../../../model/Evento';
-import {Categoria} from '../../../model/Categoria';
-import {NgbPopover} from '@ng-bootstrap/ng-bootstrap';
-import {CategoryService} from '../../../services/category/category.service';
-import {Observable} from 'rxjs';
-import {EventService} from '../../../services/event/event.service';
-import {Loadable} from '../../../model/Loadable';
-import {Router, RouterModule} from '@angular/router';
-import {IconaCategoriaMapper} from '../../../model/IconaCategoriaMapper';
-import {NgOptimizedImage} from '@angular/common';
-import {UserService} from '../../../services/user/user.service';
-import {Utente} from '../../../model/Utente';
-import {CookieService} from 'ngx-cookie-service';
-import {ToastService} from '../../../services/toast/toast.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Evento } from '../../../model/Evento';
+import { Categoria } from '../../../model/Categoria';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
+import { CategoryService } from '../../../services/category/category.service';
+import { Observable } from 'rxjs';
+import { EventService } from '../../../services/event/event.service';
+import { Loadable } from '../../../model/Loadable';
+import { Router, RouterModule } from '@angular/router';
+import { IconaCategoriaMapper } from '../../../model/IconaCategoriaMapper';
+import { NgOptimizedImage } from '@angular/common';
+import { UserService } from '../../../services/user/user.service';
+import { Utente } from '../../../model/Utente';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastService } from '../../../services/toast/toast.service';
 
 @Component({
   selector: 'app-event-card',
@@ -25,76 +25,84 @@ import {ToastService} from '../../../services/toast/toast.service';
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.css'
 })
-export class EventCardComponent implements OnInit, Loadable{
+export class EventCardComponent implements OnInit, Loadable {
   @Input() evento!: Evento
   utente!: Utente;
-  categorie :Categoria[] = [];
+  categorie: Categoria[] = [];
   loaded: boolean = false;
-  immagineEvento : string = "";
-  immagineCreatore: string ="";
+  immagineEvento: string = "";
+  immagineCreatore: string = "";
   readonly IconaCategoriaMapper = IconaCategoriaMapper;
 
 
 
   constructor(private categoryService: CategoryService,
-              private eventService: EventService,
-              private userService: UserService,
-              private cookieService: CookieService,
-              private toastService: ToastService,
-              private router: Router) {
+    private eventService: EventService,
+    private userService: UserService,
+    private cookieService: CookieService,
+    private toastService: ToastService,
+    private router: Router) {
   }
 
   ngOnInit() {
-    this.utente = JSON.parse(this.cookieService.get('utente')) ;
+    this.utente = JSON.parse(this.cookieService.get('utente'));
     this.showImageEvent();
     this.showImageCreator();
     this.showEventCategories();
   }
 
 
-  showImageCreator():void{
-    if(this.evento!==null && this.evento!==undefined)
-    {
+  showImageCreator(): void {
+    if (this.evento !== null && this.evento !== undefined) {
       this.userService.getImage(this.evento.creatore.id).subscribe(
         {
           next: (data) => {
             this.immagineCreatore = URL.createObjectURL(data);
 
           },
-          error: (error) =>{
-            this.toastService.show('errorToast',"Errore", "Impossibile recuperare l'immagine del creatore dell'evento. \n Prova a ricaricare la pagina.");
+          error: (error) => {
+            this.toastService.show('errorToast', "Errore", "Impossibile recuperare l'immagine del creatore dell'evento. \n Prova a ricaricare la pagina.");
           }
 
         }
       );
     }
   }
-
-  showImageEvent():void{
-    if(this.evento!==null && this.evento!==undefined)
-    {
-      this.eventService.getImage(this.evento.id,"1.jpg").subscribe(
+  showImageEvent(): void {
+    if (this.evento !== undefined && this.evento !== null) {
+      this.eventService.getImagesNames(this.evento.id).subscribe(
         {
           next: (data) => {
-            this.immagineEvento = URL.createObjectURL(data);
+            this.eventService.getImage(this.evento.id, data[0]).subscribe(
+              {
+                next: (d) => {
+                  this.immagineEvento = URL.createObjectURL(d);
+                },
+                error: (error) => {
+                  console.log("Errore nel recupero immagine evento card", error);
+
+                }
+
+              }
+            );
+
           },
-          error: (error) =>{
-            console.log("Errore nel recupero immagine evento card",error);
-
+          error: (err) => {
+            this.toastService.show('errorToast', 'Errore', 'Errore nel reperire le informazioni richieste.\n Prova a ricaricaricare la pagina.');
           }
-
         }
       );
     }
+
   }
 
-  showEventCategories(){
+  showEventCategories() {
     this.eventService.getCategories(this.evento.id).subscribe({
-      next: (categorie: Categoria[]) =>{
+      next: (categorie: Categoria[]) => {
         this.categorie = categorie;
         this.loaded = true;
       },
-      error:(err) =>{
+      error: (err) => {
         this.toastService.show('errorToast', 'Errore', 'Errore nel reperire le categorie di un evento.\n Prova a ricaricaricare la pagina.');
       }
     })
@@ -104,7 +112,7 @@ export class EventCardComponent implements OnInit, Loadable{
     return this.loaded;
   }
 
-  navigaDettagliEvento(idEvento:number){
+  navigaDettagliEvento(idEvento: number) {
     this.router.navigate([`/event-details/${idEvento}`]);
   }
 }
